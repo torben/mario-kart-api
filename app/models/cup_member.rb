18e3d5f10df9ded7e19514bbf0a381159c2c8c3f
Torben <toepper@redrauscher.de>
2014-01-22 18:15:18 +0100

    fixes

--- app/models/cup_member.rb
+++ app/models/cup_member.rb
@@ -4,9 +4,25 @@ class CupMember < ActiveRecord::Base
   belongs_to :vehicle
   belongs_to :character
 
+  after_create :send_apn
+
   scope :completed, -> { where('placement IS NOT NULL').where('vehicle_id IS NOT NULL').where('character_id IS NOT NULL') }
 
   symbolize :state, in: [:invited, :rejected, :accepted, :completed], scopes: true
 
   validates :state, presence: true
+
+  def send_apn
+    tokens = user.user_devices.map(&:apn_token).uniq
+    return if tokens.blank?
+
+    Thread.new do
+      payload = {:aps => {:alert => "Los jetzt. Es gibt ein Rennen zu fahren.", :sound => 'annoying_beep'}}
+      apn = []
+      for token in tokens
+        apn.push KwAPN::Notification.create(token, payload)
+      end
+      status, ret = KwAPN::Sender.push(apn, 'TestSession')
+    end
+  end
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :scm, :git
 
 # Default value for :linked_files is []
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system config/certs}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads public/assets config/certs}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
